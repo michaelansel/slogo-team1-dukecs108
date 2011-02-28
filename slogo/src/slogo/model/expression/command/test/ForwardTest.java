56b3858271ce5acd757b6a70c04006eadea4c824
Michael Ansel <michaelansel@users.noreply.github.com>
2011-02-28 12:19:37 +0000

    Update Parser to work with "fd 50"; Fix line length miscalculation

---
+++ slogo/src/slogo/model/expression/command/test/ForwardTest.java
@@ -0,0 +1,44 @@
+/**
+ * 
+ */
+package slogo.model.expression.command.test;
+
+import static org.junit.Assert.*;
+import org.junit.Before;
+import org.junit.Test;
+import slogo.model.arena.Arena;
+import slogo.model.expression.Expression;
+import slogo.model.parser.SlogoParser;
+import util.parser.ParserException;
+import util.parser.ParserResult;
+
+/**
+ * @author Michael Ansel
+ *
+ */
+public class ForwardTest
+{
+
+    /**
+     * @throws java.lang.Exception
+     */
+    @Before
+    public void setUp () throws Exception
+    {}
+
+
+    /**
+     * Test method for {@link slogo.model.expression.command.Forward#evaluate(slogo.model.arena.Arena)}.
+     * @throws ParserException 
+     */
+    @Test
+    public final void testEvaluate () throws ParserException
+    {
+        ParserResult result = SlogoParser.parse("fd 50");
+        Expression expression = (Expression) result.getList().get(0);
+        Arena arena = new Arena(null);
+        arena.addTurtle();
+        assertEquals(50, expression.evaluate(arena));
+    }
+
+}
