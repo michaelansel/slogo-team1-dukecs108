3464164a196c09a57993f4370531fa98165d9695
austinbenesh@gmail.com <austinbenesh@gmail.com@6ae9371b-8537-0621-a2f3-e18db2c9fcf4>
2011-02-28 07:37:28 +0000

    frame is getting

--- trunk/slogo/src/slogo/view/panel/HistoryPanel.java
+++ trunk/slogo/src/slogo/view/panel/HistoryPanel.java
@@ -2,6 +2,10 @@ package slogo.view.panel;
 
 import javax.swing.JPanel;
 
+/**
+ * @author Austin Benesh
+ *
+ */
 public class HistoryPanel extends JPanel
 {
 	public HistoryPanel()
--- trunk/slogo/src/slogo/view/panel/TextPanel.java
+++ trunk/slogo/src/slogo/view/panel/TextPanel.java
@@ -2,6 +2,10 @@ package slogo.view.panel;
 
 import javax.swing.JPanel;
 
+/**
+ * @author Austin Benesh
+ *
+ */
 public class TextPanel extends JPanel
 {
 	public TextPanel()
