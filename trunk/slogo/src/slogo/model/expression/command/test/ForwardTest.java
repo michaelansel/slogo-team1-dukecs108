d6194aac0497b2e84b0b7b1d8119c3f9edc95970
michael.ansel@gmail.com <michael.ansel@gmail.com@6ae9371b-8537-0621-a2f3-e18db2c9fcf4>
2011-03-03 09:28:41 +0000

    Add tests for Expression tree

--- trunk/slogo/src/slogo/model/expression/command/test/ForwardTest.java
+++ trunk/slogo/src/slogo/model/expression/command/test/ForwardTest.java
@@ -3,7 +3,7 @@
  */
 package slogo.model.expression.command.test;
 
-import static org.junit.Assert.*;
+import junit.framework.TestCase;
 import org.junit.Before;
 import org.junit.Test;
 import slogo.model.arena.Arena;
@@ -12,40 +12,57 @@ import slogo.model.parser.SlogoParser;
 import util.parser.ParserException;
 import util.parser.ParserResult;
 
+
 /**
  * @author Michael Ansel
- * 
  */
-public class ForwardTest {
+public class ForwardTest extends TestCase
+{
 
-	/**
-	 * @throws java.lang.Exception
-	 */
-	@Before
-	public void setUp() throws Exception {
-	}
+    private Arena arena;
 
-	/**
-	 * Test method for
-	 * {@link slogo.model.expression.command.Forward#evaluate(slogo.model.arena.Arena)}
-	 * .
-	 * 
-	 * @throws ParserException
-	 */
-	@Test
-	public final void testEvaluate() throws ParserException {
-	    ParserResult result;
-        Expression expression;
-	    Arena arena = new Arena();
+
+    @Before
+    public void setUp () throws Exception
+    {
+        arena = new Arena();
         arena.setCurrentTurtleID(arena.addTurtle());
-        
-		result = SlogoParser.parse("fd fd 50");
-		expression = (Expression) result.getList().get(0);
-		assertEquals(50, expression.evaluate(arena));
-		
-		result = SlogoParser.parse("fd -50");
-        expression = (Expression) result.getList().get(0);
+    }
+
+
+    @Test
+    public final void testConstantDistance () throws ParserException
+    {
+        ParserResult result = SlogoParser.parse("fd 50");
+        Expression expression = (Expression) result.getList().get(0);
         assertEquals(50, expression.evaluate(arena));
-	}
+    }
+
+
+    @Test
+    public final void testComputedDistance () throws ParserException
+    {
+        ParserResult result = SlogoParser.parse("fd 5+10");
+        Expression expression = (Expression) result.getList().get(0);
+        assertEquals(15, expression.evaluate(arena));
+    }
+
+
+    @Test
+    public final void testCommandAsParameter () throws ParserException
+    {
+        ParserResult result = SlogoParser.parse("fd fd 50");
+        Expression expression = (Expression) result.getList().get(0);
+        assertEquals(50, expression.evaluate(arena));
+    }
+
+
+    @Test
+    public final void testNegativeDistance () throws ParserException
+    {
+        ParserResult result = SlogoParser.parse("fd -50");
+        Expression expression = (Expression) result.getList().get(0);
+        assertEquals(50, expression.evaluate(arena));
+    }
 
 }
---
+++ trunk/slogo/src/slogo/model/expression/test/TestHelper.java
@@ -0,0 +1,10 @@
+package slogo.model.expression.test;
+
+
+
+public class TestHelper
+{
+
+    
+
+}
