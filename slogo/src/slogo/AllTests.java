700ffc7e3e9a025802bce5499f4dee646f1aa385
Michael Ansel <michaelansel@users.noreply.github.com>
2011-03-03 09:31:47 +0000

    Add top-level test suite containing all test cases

---
+++ slogo/src/slogo/AllTests.java
@@ -0,0 +1,24 @@
+package slogo;
+
+import junit.framework.Test;
+import junit.framework.TestSuite;
+
+
+public class AllTests
+{
+
+    public static Test suite ()
+    {
+        TestSuite suite = new TestSuite(AllTests.class.getName());
+        suite.addTest(slogo.model.expression.binary.test.AllTests.suite());
+        suite.addTest(slogo.model.expression.command.test.AllTests.suite());
+        suite.addTest(slogo.model.expression.test.AllTests.suite());
+        suite.addTest(slogo.model.parser.test.AllTests.suite());
+        suite.addTest(util.parser.grammar.test.AllTests.suite());
+        //$JUnit-BEGIN$
+
+        //$JUnit-END$
+        return suite;
+    }
+
+}
