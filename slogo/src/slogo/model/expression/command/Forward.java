40cbed485c83de2bc5e68e5c66d35dc24417ee85
Michael Ansel <michaelansel@users.noreply.github.com>
2011-03-03 09:28:09 +0000

    Implement Arithmetic Expressions, Constant, and Variable

--- slogo/src/slogo/model/expression/command/Forward.java
+++ slogo/src/slogo/model/expression/command/Forward.java
@@ -3,29 +3,62 @@
  */
 package slogo.model.expression.command;
 
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.List;
 import slogo.model.arena.Arena;
 import slogo.model.expression.Expression;
 import util.parser.ParserResult;
 import util.parser.Token;
 
+
 /**
  * @author Michael Ansel
  */
-public class Forward extends Command {
+public class Forward extends Command
+{
 
-	private Expression myDistanceExpression;
+    private Expression myDistanceExpression;
 
-	public Forward(ParserResult result) {
-		super(result);
-		// fd,whitespace,expression
-		System.out.println(result.getList().toString());
-		myDistanceExpression = null;
-	}
 
-	@Override
-	public int evaluate(Arena arena) {
-		return arena.getCurrentTurtle().move(
-				myDistanceExpression.evaluate(arena));
-	}
+    public Forward (ParserResult result)
+    {
+        // <fd>,<whitespace>,expression
+        this((Expression) result.getList().get(2));
+    }
+
+
+    public Forward (Expression distanceExpression)
+    {
+        System.out.println("Creating Forward Expression: " +
+                           distanceExpression.toString());
+        myDistanceExpression = distanceExpression;
+    }
+
+
+    @Override
+    public int evaluate (Arena arena)
+    {
+        System.out.println("Evaluating: " + this.toString());
+        int val = myDistanceExpression.evaluate(arena);
+        System.out.println("SubExpression: " + val);
+        int retval = arena.getCurrentTurtle().move(val);
+        System.out.println("Returning: " + retval);
+        return retval;
+    }
+
+
+    @Override
+    protected Collection<Expression> getExpressions ()
+    {
+        return Arrays.asList(new Expression[] { myDistanceExpression });
+    }
+
+
+    @Override
+    public String toString ()
+    {
+        return String.format("Forward(%s)", myDistanceExpression);
+    }
 
 }
