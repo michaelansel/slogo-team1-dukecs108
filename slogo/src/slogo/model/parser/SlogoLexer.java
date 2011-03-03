5e2c0f7831069b782acdb1be14def46b32e202af
Michael Ansel <michaelansel@users.noreply.github.com>
2011-03-03 09:30:23 +0000

    Reorganize Slogo parser

--- slogo/src/slogo/model/parser/SlogoLexer.java
+++ slogo/src/slogo/model/parser/SlogoLexer.java
@@ -7,34 +7,49 @@ import util.parser.AbstractLexer;
 import util.parser.ITokenRule;
 import util.parser.StringLexer;
 
+
 /**
  * @author Michael Ansel
  */
-public class SlogoLexer extends StringLexer {
-	public static final class Token {
-		public static ITokenRule ArithmeticOperator;
-		public static ITokenRule AssignmentOperator;
-		public static ITokenRule BeginExpressionGroup;
-		public static ITokenRule BeginParameterGroup;
-		public static ITokenRule BooleanOperator;
-		public static ITokenRule Command;
-		public static ITokenRule Constant;
-		public static ITokenRule EndExpressionGroup;
-		public static ITokenRule EndParameterGroup;
-		public static ITokenRule[] rules;
-		public static ITokenRule Variable;
-		public static ITokenRule Whitespace;
-		static {
-			rules = AbstractLexer.getRulesArray(Token.class);
-		}
-	};
+public class SlogoLexer extends StringLexer
+{
+    public static final class Token
+    {
+        public static ITokenRule AssignmentOperator;
+        public static ITokenRule BeginExpressionGroup;
+        public static ITokenRule BeginParameterGroup;
+        public static ITokenRule CommandName;
+        public static ITokenRule Constant;
+        public static ITokenRule EndExpressionGroup;
+        public static ITokenRule EndParameterGroup;
+        public static ITokenRule[] rules;
+        public static ITokenRule Variable;
+        public static ITokenRule Whitespace;
+        public static ITokenRule Exponent;
+        public static ITokenRule Multiply;
+        public static ITokenRule Divide;
+        public static ITokenRule Modulus;
+        public static ITokenRule Add;
+        public static ITokenRule Subtract;
+        public static ITokenRule Not;
+        public static ITokenRule LessThan;
+        public static ITokenRule GreaterThan;
+        public static ITokenRule And;
+        public static ITokenRule Or;
+        static
+        {
+            rules = AbstractLexer.getRulesArray(Token.class);
+        }
+    };
 
-	/**
-	 * @param input
-	 */
-	public SlogoLexer(String input) {
-		super(input);
-		setTokenRules(Token.rules);
-	}
+
+    /**
+     * @param input
+     */
+    public SlogoLexer (String input)
+    {
+        super(input);
+        setTokenRules(Token.rules);
+    }
 
 }
--- slogo/src/slogo/model/parser/SlogoParser.java
+++ slogo/src/slogo/model/parser/SlogoParser.java
@@ -9,7 +9,12 @@ import java.util.ResourceBundle;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 import slogo.model.arena.Arena;
+import slogo.model.expression.Constant;
 import slogo.model.expression.Expression;
+import slogo.model.expression.Variable;
+import slogo.model.expression.binary.Arithmetic;
+import slogo.model.expression.binary.BinaryExpression;
+import slogo.model.expression.command.Command;
 import util.parser.AbstractParser;
 import util.parser.AbstractParserRule;
 import util.parser.IResultHandler;
@@ -18,99 +23,56 @@ import util.parser.ParserResult;
 import util.parser.Token;
 import util.parser.grammar.GrammarParserFactory;
 
+
 /**
  * @author Michael Ansel
  */
-public class SlogoParser {
-	protected static final ResourceBundle SlogoSyntax = ResourceBundle
-			.getBundle("slogo.model.parser.SlogoGrammar");
-	protected static final ResourceBundle SlogoCommandSyntax = ResourceBundle
-			.getBundle("slogo.model.parser.SlogoCommandGrammar");
-	protected static final ResourceBundle SlogoCommandClasses = ResourceBundle
-			.getBundle("slogo.model.parser.SlogoCommandClasses");
-	protected static GrammarParserFactory commandParserFactory;
-	protected static GrammarParserFactory parserFactory;
+public class SlogoParser
+{
+    protected static final ResourceBundle SlogoSyntax =
+        ResourceBundle.getBundle("slogo.model.parser.SlogoGrammar");
+    protected static GrammarParserFactory parserFactory;
 
-	static {
-		try {
-			parserFactory = new GrammarParserFactory(SlogoSyntax);
-			commandParserFactory = new GrammarParserFactory(SlogoCommandSyntax);
-		} catch (ParserException e) {
-			e.printStackTrace();
-			throw new RuntimeException("Failed to load grammar: "
-					+ e.toString());
-		}
-	}
+    static
+    {
+        try
+        {
+            parserFactory = new GrammarParserFactory(SlogoSyntax);
+            // TODO Either ResourceBundle or final,static array
+            parserFactory.setHandler("ArithmeticExpression",
+                                     Arithmetic.getParserResultHandler());
+            parserFactory.setHandler("BooleanExpression",
+                                     slogo.model.expression.binary.Boolean.getParserResultHandler());
+            parserFactory.setHandler("Constant",
+                                     Constant.getParserResultHandler());
+            parserFactory.setHandler("Variable",
+                                     Variable.getParserResultHandler());
+            parserFactory.setHandler("Command",
+                                     Command.getParserResultHandler());
+            parserFactory.setHandler("IgnoreWhitespace", new IResultHandler()
+            {
+                @Override
+                public ParserResult handleResult (ParserResult result)
+                    throws ParserException
+                {
+                    // Don't return any matched whitespace tokens
+                    return new ParserResult();
+                }
+            });
+        }
+        catch (ParserException e)
+        {
+            e.printStackTrace();
+            throw new RuntimeException("Failed to load grammar: " +
+                                       e.toString());
+        }
+    }
 
-	public static ParserResult parse(String input) throws ParserException {
-		AbstractParser parser = parserFactory.create(new SlogoLexer(input));
-		parser.getRule("Command").setHandler(new IResultHandler() {
 
-			@Override
-			public ParserResult handleResult(ParserResult result)
-					throws ParserException {
-				CommandLexer lexer = new CommandLexer(result.getList());
-				System.out.println("Untranslated Tokens: " + result.getList());
-				System.out.println("Translated Tokens: "
-						+ lexer.tokenize().toString());
-				for (int i = 0; i < 20; i++)
-					System.out.println();
-				AbstractParser commandParser = commandParserFactory
-						.create(lexer);
-				Matcher m = Pattern.compile("FirstOf[(](.*)[)]").matcher(
-						SlogoCommandSyntax.getString("Command"));
-				m.matches();
-				String[] ruleNames = m.group(1).split("\\s*,\\s*");
-				for (final String ruleName : ruleNames) {
-					System.out
-							.println("Adding Handler to Command: " + ruleName);
-					commandParser.getRule(ruleName).setHandler(
-							new IResultHandler() {
+    public static ParserResult parse (String input) throws ParserException
+    {
+        AbstractParser parser = parserFactory.create(new SlogoLexer(input));
 
-								@Override
-								public ParserResult handleResult(
-										ParserResult result)
-										throws ParserException {
-									try {
-										return new ParserResult(
-												Class.forName(
-														SlogoCommandClasses
-																.getString(ruleName))
-														.getConstructor(
-																ParserResult.class)
-														.newInstance(result));
-									} catch (Exception e) {
-										e.printStackTrace();
-										throw new ParserException(e.toString());
-									}
-								}
-							});
-				}
-
-				result = commandParser.run();
-				return result;
-			}
-
-		});
-		parser.getRule("Expression").setHandler(new IResultHandler() {
-
-			@Override
-			public ParserResult handleResult(final ParserResult result)
-					throws ParserException {
-				return new ParserResult(new Expression() {
-					@Override
-					public String toString() {
-						return "FakeExpression<" + result.toString() + ">";
-					}
-
-					@Override
-					public int evaluate(Arena arena) {
-						return 0;
-					}
-				});
-			}
-
-		});
-		return parser.run();
-	}
+        return parser.run();
+    }
 }
