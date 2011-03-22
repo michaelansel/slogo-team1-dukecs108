651e0b2683617fb27b8f89a6f9627c39e8c9db50
Altfiv3 <Altfiv3@6ae9371b-8537-0621-a2f3-e18db2c9fcf4>
2011-03-22 05:13:18 +0000

    "Final" version- contains updated AnimatedSwingDraw, so that everything draws correctly, smoothly,e tc.

--- slogo/src/slogo/view/AnimatedSwingDraw.java
+++ slogo/src/slogo/view/AnimatedSwingDraw.java
@@ -2,13 +2,16 @@ package slogo.view;
 
 import java.awt.Graphics;
 import java.awt.Graphics2D;
+import java.awt.geom.AffineTransform;
 import java.awt.geom.Point2D;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.util.HashMap;
 import java.util.Map;
 
+import javax.swing.ImageIcon;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
 
 import slogo.model.action.Action;
@@ -16,6 +19,7 @@ import slogo.model.action.DrawRoutines;
 import slogo.model.arena.turtle.Turtle;
 import slogo.util.Position;
 import slogo.util.drawables2D.Line;
+import slogo.view.gui.panel.subpanels.ArenaDraw;
 
 
 public class AnimatedSwingDraw implements DrawRoutines
@@ -31,18 +35,22 @@ public class AnimatedSwingDraw implements DrawRoutines
     private Map<Integer, TurtleGhost> myGhosts;
     private int myDrawSpeed;
     private JPanel myPanel;
+    private BufferedImage myImage;
+    private BufferedImage myState;
 
 
-    public AnimatedSwingDraw (JPanel panel, int drawSpeed)
+    public AnimatedSwingDraw (JPanel panel, int drawSpeed, BufferedImage tImg, BufferedImage state)
     {
-        this((Graphics2D)panel.getGraphics(), drawSpeed);
+        this((Graphics2D)panel.getGraphics());
         myPanel=panel;
+    	myDrawSpeed=drawSpeed;
+    	myImage=tImg;
+    	myState=state;
     }
 
 
-    private AnimatedSwingDraw (Graphics2D g, int drawSpeed)
+    private AnimatedSwingDraw (Graphics2D g)
     {
-    	myDrawSpeed=drawSpeed;
         myGraphics = g;
         myGhosts = new HashMap<Integer, TurtleGhost>();
     }
@@ -50,19 +58,62 @@ public class AnimatedSwingDraw implements DrawRoutines
 
     @Override
     public void drawLine (Action action, Line line)
-    { 	int myCount=1;
-  	    try {
-  	      while (myCount<myDrawSpeed) {
-  	    	  double dX=((line.x2-line.x1)/myDrawSpeed)*myCount;
-  	    	  double dY=((line.y2-line.y1)/myDrawSpeed)*myCount;
-  	    	  Line l = new Line(line.x1, line.y1, line.x1+dX, line.y1+dY);
-  	          l.draw(myGraphics);
-  	          myCount++;
-  	          Thread.sleep(1000/myDrawSpeed);
-  	          myPanel.repaint();
-  	      }
-  	    } catch (InterruptedException ie) {
-  	    }
+    {
+    	int myCount=1;
+    	myDrawSpeed=80;
+    	BufferedImage[] slideShow=new BufferedImage[myDrawSpeed];
+
+    	//Pre-Calculate change in X and rotation of our image
+    	double dX=((line.x2-line.x1));
+    	double dY=((line.y2-line.y1));
+    	double rotation=Math.atan2(dY, dX);
+//    	System.out.println("dx,dy: "+dX+", "+dY+", "+" Rotation: "+rotation);
+    	dX=dX/myDrawSpeed;
+    	dY=dY/myDrawSpeed;
+    	 
+    	//Create "slides" to animate over
+    	for (int i=1;i<myDrawSpeed;i++){
+    		//Clear Panel
+    		BufferedImage slide = new BufferedImage(400,400,BufferedImage.TYPE_INT_ARGB);
+    		Graphics2D slideGFX = (Graphics2D) slide.getGraphics();
+
+    		//Calculate what to draw
+    		Line l = new Line(line.x1, line.y1, line.x1+dX*i, line.y1+dY*i);
+
+    		AffineTransform aTransform = new AffineTransform();
+    		aTransform.translate((int)line.x1+dX*i, (int)line.y1+dY*i);
+    		aTransform.rotate(rotation+Math.PI/2);
+    		aTransform.translate(-myImage.getWidth()/2.0, -myImage.getHeight()/2.0);
+
+    		//Draw
+			slideGFX.drawImage(myState, 0, 0, null);
+    		l.draw(slideGFX);
+    		slideGFX.drawImage(myImage, aTransform, null);
+    		i++;
+    		slideShow[i-1]=slide;
+    		slideGFX.dispose();
+    	}
+
+    	//Animate over the created slides
+    	try {
+    		
+    		
+    		while (myCount<myDrawSpeed) {
+    			//Clear Panel
+    			((ArenaDraw)myPanel).clear(myGraphics);
+
+    			//Draw our current "slide"
+    			myGraphics.drawImage(slideShow[myCount-1], 0, 0, myPanel);
+    			myCount++;
+
+    			//repaint what was drawn
+    			myPanel.repaint();
+
+    			//wait
+    			Thread.sleep(1000/40);
+    		}
+    	} catch (InterruptedException ie) {
+    	}
   	  
     }
 
@@ -78,7 +129,65 @@ public class AnimatedSwingDraw implements DrawRoutines
     @Override
     public void walk (Action action, Point2D from, Point2D to)
     {
-        getGhost(action.getTurtleID()).position.setLocation(to);
+    	//Calculate
+    	int myCount=1;
+    	myDrawSpeed=80;
+    	BufferedImage[] slideShow=new BufferedImage[myDrawSpeed];
+
+    	//Pre-Calculate change in X and rotation of our image
+    	double dX=to.getX()-from.getX();
+    	double dY=to.getY()-from.getY();
+    	double rotation=Math.atan2(dY, dX);
+//    	System.out.println("dx,dy: "+dX+", "+dY+", "+" Rotation: "+rotation);
+    	dX=dX/myDrawSpeed;
+    	dY=dY/myDrawSpeed;
+    	
+    	//Create "slides" to animate over
+    	for (int i=1;i<myDrawSpeed;i++){
+    		//Clear Panel
+    		BufferedImage slide = new BufferedImage(400,400,BufferedImage.TYPE_INT_ARGB);
+    		Graphics2D slideGFX = (Graphics2D) slide.getGraphics();
+
+            //Draw
+	    	  int x = (int)(from.getX()+dX*i);
+	    	  int y = (int)(from.getY()+dY*i);
+	          
+	        AffineTransform aTransform = new AffineTransform();
+            aTransform.translate(x,y);
+    		aTransform.rotate(rotation+Math.PI/2);
+            aTransform.translate(-myImage.getWidth() / 2.0, -myImage.getHeight() / 2.0);
+
+    		//Draw
+			slideGFX.drawImage(myState, 0, 0, null);
+            slideGFX.drawImage(myImage, aTransform, null);
+    		i++;
+    		slideShow[i-1]=slide;
+    		slideGFX.dispose();
+    	}
+    	 
+
+    	//Animate over the created slides
+    	try {
+    		
+    		
+    		while (myCount<myDrawSpeed) {
+    			//Clear Panel
+    			((ArenaDraw)myPanel).clear(myGraphics);
+
+    			//Draw our current "slide"
+    			myGraphics.drawImage(slideShow[myCount-1], 0, 0, myPanel);
+    			myCount++;
+
+    			//repaint what was drawn
+    			myPanel.repaint();
+
+    			//wait
+    			Thread.sleep(1000/40);
+    		}
+    	} catch (InterruptedException ie) {
+    	}
+
+    	//        getGhost(action.getTurtleID()).position.setLocation(to);
     }
 
 
